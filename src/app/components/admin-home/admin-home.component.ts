dd63ca73d93c221a575203df7a2248db671923b9
Isha Bhelsekar <113613452+i-s-h-a-b@users.noreply.github.com>
2026-01-21 23:29:46 +0530

    Add RouterLink import to AdminHomeComponent

--- src/app/components/admin-home/admin-home.component.ts
+++ src/app/components/admin-home/admin-home.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-admin-home',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './admin-home.component.html',
   styleUrl: './admin-home.component.css'
 })
